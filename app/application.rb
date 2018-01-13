3bca9f007faa87158fadb73e53106f47de4bbb61
ronald vilorio <soccer23415@gmail.com>
2018-01-13 21:31:32 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,9 +5,9 @@ class Application
 
     rand_time = Kernel.rand(1..24)
 
-    if rand_time.between?(1..11)
+    if rand_time.between?(1,11)
       resp.write "Good Morning!"
-    else rand_time.between?(12..24)
+    else rand_time.between?(12,24)
       resp.write "Good Afternoon"
     end
 
