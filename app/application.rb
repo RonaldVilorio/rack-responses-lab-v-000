be6182170849661949f484b04ec04f8415f42445
ronald vilorio <soccer23415@gmail.com>
2018-01-13 21:30:48 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,7 @@ class Application
 
     if rand_time.between?(1..11)
       resp.write "Good Morning!"
-    else Morning
+    else rand_time.between?(12..24)
       resp.write "Good Morning!"
     end
 
