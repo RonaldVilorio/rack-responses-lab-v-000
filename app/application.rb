2ee18911f2ba21b7016728a704767ca510668d7f
ronald vilorio <soccer23415@gmail.com>
2018-01-13 21:25:33 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
     time = Time.new
     if time.strftime("%k:%M").to_i > 12
       resp.write "Good Afternoon!"
-    elsif time.strftime("%k:%M").to_i > 12
+    elsif time.strftime("%k:%M").to_i < 12
 
       resp.write "Good Morning!"
     end
